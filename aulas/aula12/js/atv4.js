e06fb2aa2c8fdbdc5cbc13c870abaa489079e72b
EduardoxxMenezes <162368836+EduardoxxMenezes@users.noreply.github.com>
2025-04-21 12:36:44 -0300

    Delete aulas directory

--- aulas/aula12/js/atv4.js
+++
@@ -1,8 +0,0 @@
-function validaBusca(){
-    if(document.querySelector("#inputlupa").value == ""){
-        alert("Nao deixe a busca em branco!");
-    }
-    return false;
-}
-
-document.querySelector("#Form-busca").onsubmit = validaBusca;
--- aulas/aula13/script.js
+++
@@ -1,9 +0,0 @@
-let banners = ["./js/h.jpg",'./js/h1.png'];
-
-let cont = 0;
-function trocaBanner(){
-    cont = (cont +1) % 2;
-    document.querySelector(".destaque img").src=banners[cont];
-}
-
-setInterval(trocaBanner,1000);
